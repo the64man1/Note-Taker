c1e081c3819492d7689284cb8708d817aa54b791
maxquintanilla <the64man1@yahoo.com>
2021-05-09 11:44:28 -0500

    fixed script link in notes.html, put in notes array and api get handler in server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
@@ -8,7 +9,16 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const notes = [
+    {
+        "title":"Test Title",
+        "text":"Test text"
+    }
+];
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('/api/notes', (req, res) => res.json(notes));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
